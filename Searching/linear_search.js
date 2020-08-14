4e970dc232463500b71e124631eb85a93912ca10
Sharon Kim <carolinek@SKs-MacBook-Air.lan>
2020-08-14 18:15:47 -0400

    added cleaner solution to linear search problem

--- Searching/linear_search.js
+++ Searching/linear_search.js
@@ -1,16 +1,29 @@
 //LINEAR SEARCH PRACTICE
 // return -1 if it doesnt exist in array
-function linearSearch (arr, value) {
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === value) {
-        return i;
-      } 
-      if (arr.includes(value) === false) {
-        return -1;
-      }
-    }
-  }
+// function linearSearch (arr, value) {
+//     for (let i = 0; i < arr.length; i++) {
+//       if (arr[i] === value) {
+//         return i;
+//       } 
+//       if (arr.includes(value) === false) {
+//         return -1;
+//       }
+//     }
+// }
   
-  linearSearch([1,2,3,4], 3)
+// linearSearch([1,2,3,4], 3)
   
   // I tried for of loop and ternary, but they dont work bc it just wouldnt make sense since if you did return (arr[i] === value ? i : -1) it just returns -1 without going thru rest of array bc of return keyword!!!!!
+
+
+  // CLEANER SOLUTION
+const linearSearch = (arr, val) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === val) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+console.log(linearSearch([1,2,3], 6))
