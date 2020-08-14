4f492384a7d74dc1b45b8b94ae1009c6dadd6798
Sharon Kim <carolinek@SKs-MacBook-Air.lan>
2020-08-14 17:36:49 -0400

    completed more code challenges

--- index.js
+++ index.js
@@ -27,3 +27,26 @@ function findTotal(num) {
 
 console.log(findTotal(5));
 
+// ** Write a function which takes 2 numbers and returns their sum
+const sum = (num1, num2) => {
+    return num1 * num2
+}
+
+console.log(sum(5,7))
+
+// ** Write a function that takes a string and returns counts of each character in the string
+const countChar = (str) => {
+    let myArr = str.toLowerCase().split("") // to account for when user inputs uppercase letters
+    let freqChar = {};
+    for (let char of myArr) {
+        // console.log(char)
+        freqChar[char] ? freqChar[char] ++ : freqChar[char] = 1
+    }
+    return freqChar;
+}
+
+console.log(countChar("sharon"))
+console.log(countChar("suNny"))
+
+
+
