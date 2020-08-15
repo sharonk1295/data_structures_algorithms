a3802fb00a162fd172683b0ac4eda7f2dd6e30f7
Sharon Kim <carolinek@SKs-MacBook-Air-42.lan>
2020-08-15 11:29:16 -0400

    finished mutation practice

--- index.js
+++ index.js
@@ -57,5 +57,30 @@ const countChar = (str) => {
 console.log(countChar("sharon"))
 console.log(countChar("suNny"))
 
+// An object cannot be nested in another object
+// cannot declare a value of a property as the object it is in bc the obj technically hasnt been declared yet so theres nothing for the property to reference
+// let president = {
+//     name: "Pooh",
+//     next: president
+// }
 
+// console.log(president.next) // throws error: cannot access 'president' before initialization
 
+// so do this instead:
+// let president = {
+//     name: "Pooh"
+// }
+// president.next = president
+
+// console.log(president.next) // [Circular *1]
+
+// JS eventually removes a value from memory if there is no wire pointing at it
+
+// if an obj does not have a property (look before the dot in dot notation) then it is null or undefined (not pointing at an undefined value though) and then if you try to access another property on top of that undefined property, it immediately throws an error.
+
+const music = {
+    taste: 'classical'
+}
+const onion = music
+onion.taste = 'umami'
+console.log(music.taste)
