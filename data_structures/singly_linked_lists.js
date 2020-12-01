917f2e9d84805e068712fdd4ca1ecdc27624e656
Sharon Kim <carolinek@SKs-MacBook-Air.lan>
2020-11-30 21:01:54 -0500

    introduced singly linked lists

---
+++ data_structures/singly_linked_lists.js
@@ -0,0 +1,6 @@
+// ========= SINGLY LINKED LISTS ========== //
+
+// A data structure that contains a HEAD, TAIL, and LENGTH property
+// Linked Lists consists of nodes, and each node has a value and a pointer to another node or null (like a train with linked cars)
+// NO INDEX TO ACCESS THE ELEMENTS (cant ask for the 5th thing)
+// basically, starting from the head, a node points to the next one until you reach the end, which doesnt reference anything so its "points" to NULL
